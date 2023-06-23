ed2b9a549f8561dd6bbb71a67ceee4865f35876d
Angelito Arboleda <105031939+Palangge@users.noreply.github.com>
2023-06-23 19:37:14 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { getIndexPage } = require('./server/routes/routes.js');
-const userRoutes = require('./server/routes/user.js');
-const productRoutes = require('./server/routes/product.js');
-const cartRoutes = require('./server/routes/cart.js');
-const contactRoutes = require('./server/routes/contact.js');
-const path = require('path');
-const { connectToMongoDB } = require('./server/driver/mongodb.js');
+const userRoutes = require('./routes/user.js');
+const productRoutes = require('./routes/product.js');
+const cartRoutes = require('./routes/cart.js');
+const contactRoutes = require('./routes/contact.js');
 const session = require('express-session');
 const { v4: uuidv4 } = require('uuid');
 
-// Connect to MongoDB
-//connectToMongoDB();
-
 const app = express();
-const port = process.env.PORT || 3000; // Use the provided port or fallback to 3000
+const port = process.env.PORT || 3000;
 
 app.use(
   session({
@@ -24,27 +18,20 @@ app.use(
     saveUninitialized: false,
     cookie: {
       expires: 600000 // 10 mins of inactivity.
-    } 
+    }
   })
 );
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, "build")));
-app.use(express.static("public"));
-
-app.use('/users', userRoutes.userRouter);
+app.use('/api/users', userRoutes.userRouter);
 app.use('/api/products', productRoutes.productRouter);
 app.use('/api/cart', cartRoutes.cartRouter);
 app.use('/api/contact', contactRoutes.contactRouter);
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.status(404).send('Not Found');
 });
 
-app.listen(port, () => {
-  console.log('Server is running on port ' + port);
-});
-
-module.exports = app; // Export the Express app object
+module.exports = app;
